5fc23d29088a4ef3688667bebdbbae0e1097ee1c
brandon v dongen <Brandonvdongen@hotmail.com>
2018-02-09 10:54:48 +0100

    updated mapController

--- modules/mapController.js
+++ modules/mapController.js
@@ -1,4 +1,4 @@
-//
+//map data
 const map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
@@ -15,7 +15,18 @@ const map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-
+//get map as array
 export function getMap() {
     return map;
+}
+
+//get tiletype of coordinate in grid
+export function getTile(x,y){
+    const type = map[x][y];
+    if(type === 0)return "EMPTY";
+    if(type === 1)return "UNBREAKABLE";
+    if(type === 2)return "BREAKABLE";
+    if(type === 3)return "PICKUP";
+    if(type === 4)return "PLAYER";
+    if(type === 5)return "EXPLOSION";
 }
